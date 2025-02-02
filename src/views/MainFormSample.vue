17e744c5af4b7b33da1f6b2e0d71842c92f10107
saptadi73 <saptadi.nurfarid@gmail.com>
2025-02-02 14:24:33 +0700

    template habis install data-tables

--- src/views/MainFormSample.vue
+++ src/views/MainFormSample.vue
@@ -6,6 +6,7 @@
                 <NavBarPreline/>
                 <div><CardWidthFull/></div>
                 <div><CardTwoFormSample/></div>
+                <div><DataTableSinglePage/></div>
                
             </div>
         </div>
@@ -18,6 +19,7 @@ import NavBarPreline from '../components/NavBarPreline.vue';
 import CardWidthFull from '../components/CardWidthFull.vue';
 import CardGridView from '../components/CardGridView.vue';
 import CardTwoFormSample from '../components/CardTwoFormSample.vue';
+import DataTableSinglePage from '../components/DataTableSinglePage.vue';
 
 
 </script>
