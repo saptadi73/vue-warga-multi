b4bf68749250103744549dc9cbfab2cfeabaaf6d
saptadi73 <saptadi.nurfarid@gmail.com>
2025-02-16 09:45:50 +0700

    penambahan konfigurasi di Blok/KK

--- src/views/MainIuranPageView.vue
+++ src/views/MainIuranPageView.vue
@@ -11,11 +11,7 @@
 </template>
 
 <script setup>
-<<<<<<< HEAD
 import SidebarJoss from '../components/SidebarJoss.vue';
-=======
-import SidebarJoss from './SidebarJoss.vue';
->>>>>>> a4394ce363169c84fbc1e7e250c77d2a721bc878
 import NavBarPreline from '../components/NavBarPreline.vue';
 import { RouterView } from 'vue-router';
 
