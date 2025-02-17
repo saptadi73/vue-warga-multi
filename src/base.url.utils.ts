d59a7798ab7ede1a99610d59238d40e1fbe65bcf
ferd14nsyah <ferdiferdiansyah066@gmail.com>
2025-02-17 19:00:19 +0700

    update

--- src/base.url.utils.ts
+++ src/base.url.utils.ts
@@ -1,2 +1,2 @@
-// export const BASE_URL = 'http://103.127.139.73:3000/';
-export const BASE_URL = 'http://localhost:3000/';
+export const BASE_URL = 'http://103.127.139.73:3000/';
+// export const BASE_URL = 'http://localhost:3000/';
